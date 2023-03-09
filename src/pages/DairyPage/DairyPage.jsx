7cd034caabaa59ceedc5a565cf2d43652a047616
Oleksandra Tiankina <ag7756862@gmail.com>
2023-03-09 13:25:43 +0200

    delete depencies

--- src/pages/DairyPage/DairyPage.jsx
+++ src/pages/DairyPage/DairyPage.jsx
@@ -1,20 +1,5 @@
 import { DiaryAddProductForm } from 'components/DiaryAddProductForm/DiaryAddProductForm';
-import { SideBar } from 'components/SideBar/SideBar';
-import s from './DairyPage.module.css';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectDate } from 'redux/products/selectors';
-import { getProductsByDate } from 'redux/products/operations';
-
 export function DairyPage() {
-  const dispatch = useDispatch();
-  const date = useSelector(selectDate);
-
-
-  useEffect(() => {
-    if (date !== null) dispatch(getProductsByDate(date));
-  }, [date, dispatch]);
-
   return (
     <DiaryAddProductForm />
   );
